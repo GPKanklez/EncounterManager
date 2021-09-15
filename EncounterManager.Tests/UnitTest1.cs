8ff350a9f2612e2c163cd96b65bbb64cef8c418a
Cade Schlaefli <cade.schlaefli@gmail.com>
2021-09-14 21:07:19 -0500

    add Package and test projects for publishing

---
+++ EncounterManager.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EncounterManager.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
